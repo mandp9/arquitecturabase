c96458549578f4807ca849ef1e0b3d6906f31806
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-10-08 10:24:28 +0200

    actualizado index y añadida carpeta modelo.js a servidor

---
+++ servidor/modelo.js
@@ -0,0 +1,29 @@
+function Sistema(){
+  this.usuarios = {};
+  this.agregarUsuario = function(nick){
+    this.usuarios[nick] = new Usuario(nick);
+  };
+  this.obtenerUsuarios = function(){   
+    return this.usuarios;
+  };
+  this.usuarioActivo = function(nick){
+    return this.usuarios.hasOwnProperty(nick);
+  };
+  this.eliminarUsuario = function(nick){
+    if(this.usuarioActivo(nick)){
+        delete this.usuarios[nick];
+        return true;
+    }else{
+        return false;
+    }
+  };
+  this.numeroUsuarios = function(){
+    return Object.keys(this.usuarios).length;
+  };
+}
+
+function Usuario(nick){
+  this.nick = nick;
+}
+
+module.exports.Sistema = Sistema;
