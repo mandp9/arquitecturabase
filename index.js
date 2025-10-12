d5d7df4549d7cf84270cc552bce60948a8628c0e
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-10-12 12:14:45 +0200

    añadido app-get para agregar usuario

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', (request, response) => {
     response.setHeader("Content-type","text/html");
     response.send(contenido);
 });
+app.get("/agregarUsuario/:nick",function(request,response){
+let nick=request.params.nick;
+let res=sistema.agregarUsuario(nick);
+response.send(res);
+});
 
 app.listen(PORT, () => {
     console.log(`App está escuchando en el puerto ${PORT}`);
