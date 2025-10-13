aaea8279c19cd08d424c1375d1ad430de2dd0491
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-10-13 09:26:26 +0200

    mas cambios en clienteRest

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -9,5 +9,69 @@ else{
 console.log("El nick ya está ocupado");
 }
 })
-}
+};
+this.agregarUsuario2 = function (nick) {
+    $.ajax({
+      type: 'GET',
+      url: '/agregarUsuario/' + encodeURIComponent(nick),
+      dataType: 'json',               // esperamos JSON de respuesta
+      // contentType no es necesario en GET sin body
+      success: function (data) {
+        if (data.nick != -1) {
+          console.log('Usuario ' + nick + ' ha sido registrado');
+        } else {
+          console.log('El nick ya está ocupado');
+        }
+        // opcional: logSalida('agregarUsuario2', data);
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log('Status: ' + textStatus);
+        console.log('Error: ' + errorThrown);
+      }
+    });
+  };
+
+  // Si quieres, añade el resto con $.ajax también:
+  this.obtenerUsuarios = function () {
+    return $.ajax({
+      type: 'GET',
+      url: '/obtenerUsuarios',
+      dataType: 'json'
+    }).done(function (data) {
+      console.log('Usuarios:', data);
+      // logSalida('obtenerUsuarios', data);
+    }).fail(function (xhr, s, e) {
+      console.log('Error obtenerUsuarios:', s, e);
+    });
+  };
+
+  this.usuarioActivo = function (nick) {
+    return $.ajax({
+      type: 'GET',
+      url: '/usuarioActivo/' + encodeURIComponent(nick),
+      dataType: 'json'
+    }).done(function (data) {
+      console.log('Activo:', data);
+    });
+  };
+
+  this.numeroUsuarios = function () {
+    return $.ajax({
+      type: 'GET',
+      url: '/numeroUsuarios',
+      dataType: 'json'
+    }).done(function (data) {
+      console.log('Num:', data);
+    });
+  };
+
+  this.eliminarUsuario = function (nick) {
+    return $.ajax({
+      type: 'GET',
+      url: '/eliminarUsuario/' + encodeURIComponent(nick),
+      dataType: 'json'
+    }).done(function (data) {
+      console.log('Eliminado:', data);
+    });
+  };
 }
