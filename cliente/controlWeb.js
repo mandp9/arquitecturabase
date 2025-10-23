3559b427d8fed36da9a0dfca9197be66c6e7edd8
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-10-23 10:54:46 +0200

    2.1.2 Implementar localStorage

--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,4 +1,21 @@
 function ControlWeb() {
+  this.pintarMenu = function(nick) {
+    const $menu = $("#menu"); // <ul id="menu"> en index.html
+    if (nick) {
+      $menu.html(`
+        <li class="nav-item"><a class="nav-link" href="#home">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="#link2">Link 2</a></li>
+        <li class="nav-item"><a id="lnkSalir" class="nav-link" href="#">Salir</a></li>
+      `);
+      $("#lnkSalir").on("click", (e) => { e.preventDefault(); this.salir(); });
+    } else {
+      $menu.html(`
+        <li class="nav-item"><a class="nav-link" href="#login">Inicio sesión</a></li>
+        <li class="nav-item"><a class="nav-link" href="#link2">Link 2</a></li>
+        <li class="nav-item disabled"><a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Salir</a></li>
+      `);
+    }
+  };
   this.mostrarAgregarUsuario = function () {
     var html = '';
     html += '<div id="mAU">';
@@ -17,4 +34,31 @@ function ControlWeb() {
       rest.agregarUsuario(nick);               // llama a tu endpoint
     });
   };
+  this.comprobarSesion=function(){
+    let nick=localStorage.getItem("nick");
+    this.pintarMenu(nick);
+    if (nick){
+      this.mostrarMensaje("Bienvenido al sistema, "+nick);
+    }
+    else{
+      this.mostrarAgregarUsuario();
+    }
+  };
+  this.mostrarMensaje = function (msg) {
+  const html = `
+    <div class="alert alert-info" role="alert">
+      ${msg}
+    </div>
+  `;
+  $('#au').html(html);
+   $('#btnSalir').on('click', () => {
+    this.salir(); 
+  });
+  };
+  this.salir=function(){
+    const nick = localStorage.getItem('nick');
+    localStorage.removeItem("nick");
+    this.pintarMenu(null);
+    this.mostrarMensaje(`¡Hasta luego${nick ? ', ' + nick : ''}!`);
+    setTimeout(() => location.reload(), 1200); };
 }
