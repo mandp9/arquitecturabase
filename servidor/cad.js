11a34db01b638c4079442fd46ad2e3a9f40f603a
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-11-04 18:44:26 +0100

    conseguida conexión bbdd

--- servidor/cad.js
+++ servidor/cad.js
@@ -6,7 +6,7 @@ function CAD(){
     this.conectar=async function(callback) {
     let cad=this;
     let client= new
-    mongo ("mongodb+srv://mandp9:<Google9>@cluster0.3wzaezh.mongodb.net/?appName=Cluster0"); // Reemplaza 'xxxx'
+    mongo ("mongodb+srv://mandp9:temp1234@cluster0.3wzaezh.mongodb.net/?appName=Cluster0"); // Reemplaza 'xxxx'
     await client.connect(); // Establece la conexión
     const database=client.db("sistema"); // Conéctate o crea la base de datos "sistema"
     cad.usuarios=database.collection("usuarios"); // Asigna la colección "usuarios"
