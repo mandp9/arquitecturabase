ec9fed9a0d3ba54893a5a9cac93c2dd95d09c0ee
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-11-04 10:58:09 +0100

    autenticacion google pre-foto funcionando

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const modelo = require("./servidor/modelo.js");
 
 const PORT = process.env.PORT || 3000; //puerto que utilizara para escuchar
 
-app.use(express.static(__dirname + "/")); //carga middleware
-let sistema = new modelo.Sistema();
-
 app.use(cookieSession({
  name: 'Sistema',
  keys: ['key1', 'key2']
@@ -19,7 +16,29 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+let sistema = new modelo.Sistema();
+
 app.get("/auth/google",passport.authenticate('google', { scope: ['profile','email'] }));
+
+app.get('/google/callback',
+ passport.authenticate('google', { failureRedirect: '/fallo' }),
+ function(req, res) {
+ res.redirect('/good');
+});
+
+app.get("/good", function(request,response){
+ let nick=request.user.emails[0].value;
+ if (nick){
+ sistema.agregarUsuario(nick);
+ }
+ //console.log(request.user.emails[0].value);
+ response.cookie('nick',nick);
+ response.redirect('/');
+});
+
+app.get("/fallo",function(request,response){
+ response.send({nick:"nook"})
+});
 
 app.get('/', (request, response) => {
   var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
@@ -57,6 +76,9 @@ app.get('/eliminarUsuario/:nick', function (req, res) {
   var ok = sistema.eliminarUsuario(nick);
   res.json({ ok: ok }); // {ok:true/false}
 });
+
+app.use(express.static(__dirname + "/"));
+
 app.listen(PORT, () => {
     console.log(`App está escuchando en el puerto ${PORT}`);
     console.log('Ctrl+C para salir');
