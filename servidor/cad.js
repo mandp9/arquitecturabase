3c0a5dc756e8d34e1c391ae59226ab5662d86b34
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-11-06 12:54:19 +0100

    Corregido test en modelo.js

--- servidor/cad.js
+++ servidor/cad.js
@@ -4,13 +4,29 @@ const ObjectId = require("mongodb").ObjectId;
 function CAD(){
     this.usuarios;
     this.conectar=async function(callback) {
-    let cad=this;
-    let client= new
-    mongo ("mongodb+srv://mandp9:temp1234@cluster0.3wzaezh.mongodb.net/?appName=Cluster0"); // Reemplaza 'xxxx'
-    await client.connect(); // Establece la conexión
-    const database=client.db("sistema"); // Conéctate o crea la base de datos "sistema"
-    cad.usuarios=database.collection("usuarios"); // Asigna la colección "usuarios"
-    callback(database); // Ejecuta el callback
-}
+        let cad=this;
+        let client= new
+        mongo ("mongodb+srv://mandp9:temp1234@cluster0.3wzaezh.mongodb.net/?appName=Cluster0"); // Reemplaza 'xxxx'
+        await client.connect(); // Establece la conexión
+        const database=client.db("sistema"); // Conéctate o crea la base de datos "sistema"
+        cad.usuarios=database.collection("usuarios"); // Asigna la colección "usuarios"
+        callback(database); // Ejecuta el callback
+    }
+    this.buscarOCrearUsuario=function(usr,callback){
+        buscarOCrear(this.usuarios,usr,callback);
+    }
+    function buscarOCrear(coleccion,criterio,callback)
+    {
+        coleccion.findOneAndUpdate(criterio, {$set: criterio}, {upsert:
+        true,returnDocument:"after",projection:{email:1}}, function(err,doc) {
+        if (err) { throw err; }
+        else {
+            console.log("Elemento actualizado");
+            console.log(doc.value.email);
+            callback({email:doc.value.email});
+        }
+        });
+    }
+
 }
 module.exports.CAD=CAD;
--- servidor/modelo.js
+++ servidor/modelo.js
@@ -3,10 +3,13 @@ const datos=require("./cad.js");
 function Sistema(test){
   this.usuarios = {};
   this.cad=new datos.CAD();
-  this.cad.conectar(function(db){
-        console.log("Conectado a Mongo Atlas");
-    });
-    
+ 
+  if(!test.test){
+    this.cad.conectar(function(db){
+      console.log("Conectado a Mango Atlas");});
+      //Aqui puedes realizar operaciones con la base de datos
+  }
+
   this.agregarUsuario = function(nick){
     let res={"nick":-1};
     if (!this.usuarios[nick]){
@@ -35,15 +38,15 @@ function Sistema(test){
   this.numeroUsuarios = function(){
     return Object.keys(this.usuarios).length;
   };
+  this.usuarioGoogle=function(usr,callback){
+    this.cad.buscarOCrearUsuario(usr,function(obj){
+    callback(obj);
+  });
+  };
 }
 
 function Usuario(nick){
   this.nick = nick;
-  if(!test.test){
-    this.cad.conectar(function(db){
-      console.log("Conectado a Mango Atlas");});
-      //Aqui puedes realizar operaciones con la base de datos
-  }
 }
 
 
