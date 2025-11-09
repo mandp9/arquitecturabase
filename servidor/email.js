820aff9b34ae6984f1d5a8f5cd653f96ab56180d
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-11-09 21:24:05 +0100

    corregido dominio

--- servidor/email.js
+++ servidor/email.js
@@ -16,6 +16,5 @@ module.exports.enviarEmail = async function(direccion, key, men) {
     to: direccion, 
     subject: men, 
     text: 'Pulsa aquí para confirmar cuenta', 
-    html: '<p>Bienvenido a Sistema</p><p><a href="' + url + 'confirmarUsuario/' + direccion + '/' + key + '">Pulsa aquí para confirmar cuenta</a></p>' 
-  });
+    html: '<p>Bienvenido a Sistema</p><p><a href="' + url + '/confirmarUsuario/' + direccion + '/' + key + '">Pulsa aquí para confirmar cuenta</a></p>'  });
 }
