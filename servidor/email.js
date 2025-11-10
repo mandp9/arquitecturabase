df4404bb9388e3b48bbb92489cdbf82de13726a7
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-11-10 12:08:41 +0100

    prueba

--- servidor/email.js
+++ servidor/email.js
@@ -18,3 +18,4 @@ module.exports.enviarEmail = async function(direccion, key, men) {
     text: 'Pulsa aquí para confirmar cuenta', 
     html: '<p>Bienvenido a Sistema</p><p><a href="' + url + '/confirmarUsuario/' + direccion + '/' + key + '">Pulsa aquí para confirmar cuenta</a></p>'  });
 }
+//prueba 
